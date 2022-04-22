89ad92000c920efda0cff82dbdad21a85be633be
Tiffany <leopardmitai@gmail.com>
2022-04-22 16:59:32 -0700

    added import files to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import SearchBooks from './pages/SearchBooks';
-import SavedBooks from './pages/SavedBooks';
-import Navbar from './components/Navbar';
+
+import React from "react";
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+
+import SavedBooks from "./pages/SavedBooks";
+import SearchBooks from "./pages/SearchBooks";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
