0549371ad1eabbc80f067bfd56a8742110c9ac17
Tiffany <leopardmitai@gmail.com>
2022-04-22 17:14:51 -0700

    Started schema file

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 
 import React from "react";
-import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from "@apollo/client";
+import { ApolloClient, InMemoryCache, ApolloProvider, createLink } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -8,6 +8,25 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SavedBooks from "./pages/SavedBooks";
 import SearchBooks from "./pages/SearchBooks";
 import Navbar from "./components/Navbar";
+
+const newLink = createLink({
+  url: '/graphql',
+});
+
+const auth = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const apolloClient = new ApolloClient({
+  link: auth.concat(newLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
