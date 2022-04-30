bcaa574441892a526e64eb3f027e7fc1b2232356
Tiffany <leopardmitai@gmail.com>
2022-04-29 17:18:54 -0700

    troubleshooting, got graphql server to connect!

--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -23,16 +23,25 @@ const typeDefs = gql`
     user: User
   }
 
+  input bookInput {
+    bookId: ID
+    title: String
+    authors: [String]
+    link: String
+    image: String
+    description: String
+  }
+
   type Query {
     me: User
     users: [User]
   }
 
   type Mutation {
-    createUser 
+    createUser(username: String!, email: String!, password: String!): Auth
     login(password: String!, email: String!): Auth
-    saveBook
-    deleteBook
+    saveBook(BookData: bookInput):User
+    deleteBook(bookId: ID!): User
   }
 `;
 
